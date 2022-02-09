74fa82189287dc303edc2d89a62cd63aef194b1b
hnazmul <nazmul.w3@gmail.com>
2022-02-09 13:37:06 +0600

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Header from "./Components/Header.svelte";
+</script>
+
+<main>
+    <Header />
+    <h1>Hello Nazmul!</h1>
+</main>
+
+<style lang="scss">
+    main{
+        max-width: 800px;
+        margin: auto;
+    }
+</style>
---
+++ src/Components/Header.svelte
@@ -0,0 +1,19 @@
+<header id="header">
+    <h1>
+        <img src="/img/poll_ninja_logo.svg" class="poll-ninja-logo" alt="" />
+    </h1>
+</header>
+
+<style lang="scss">
+    #header {
+        background: white;
+        h1 {
+            margin: 0;
+            .poll-ninja-logo {
+                width: 300px;
+                display: block;
+                margin: auto;
+            }
+        }
+    }
+</style>
